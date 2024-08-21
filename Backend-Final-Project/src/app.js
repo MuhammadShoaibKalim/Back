412f6cb263ddd9810bd7e7bfaaf8d3d61e1d91d7
Muhammad Shoaib Kalim <mmuhammadshoaibkalim@gmail.com>
2024-08-21 23:59:34 +0500

    changes

--- Backend-Final-Project/src/app.js
+++ Backend-Final-Project/src/app.js
@@ -6,11 +6,10 @@ const app = express();
 
 
 
-app.use(cors({
-    origin: process.env.CORS_ORIGIN,
-    credentials: true,
-}));
-
+// app.use(cors({
+//     origin: process.env.CORS_ORIGIN,
+//     credentials: true,
+// }));
 
 app.use(express.json({limit:"16kb"}));
 app.use(express.urlencoded({extended:true, limit:"16kb"}));
